ce76bb33309dbf336f2c85d207bb780e9f42ace9
Craig Rash Jr <90971454+GitCraigRash@users.noreply.github.com>
2024-01-07 21:43:54 -0500

    Update containsDuplicate.py

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -3,3 +3,7 @@ class Solution:
         if len(set(nums)) != len(nums):
             return True
         return False
+
+SELECT user_id, COUNT(follower_id) as followers_count FROM Followers
+Group by user_id
+ORDER by user_id ASC;
